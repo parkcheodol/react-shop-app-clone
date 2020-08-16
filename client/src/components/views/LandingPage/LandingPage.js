ae27f16cf7ceb2d709c2a7353736466aaa7a57e0
parkcheodol <3611345@naver.com>
2020-08-16 22:39:56 +0900

    200816 #3-2 Cards

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,13 +1,17 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import { FaCode } from "react-icons/fa";
+import { Icon, Col, Card, Row } from 'antd';
+import Meta from 'antd/lib/card/Meta'
+
 
 function LandingPage() {
+
+    const [Products, setProducts] = useState([])
 
     // #3-1 DB에 저장된 것을 가져오기
     useEffect(() => {
 
-        
         
         // Backend 에 요청 보내기 
         axios.post('/api/product/products')
@@ -15,6 +19,7 @@ function LandingPage() {
                 if(response.data.success) {
                     // front에서 정보를 받아봄 
                     console.log(response.data)
+                    setProducts(response.data.productInfo)
                 } else {
                     alert('상품 가져오기 실패')
                 }
@@ -23,11 +28,31 @@ function LandingPage() {
         
     }, [])
 
+    // #3-2 Product Cards
+    const renderCards = Products.map((product, index) => {
+
+        // 카드에 어떤 정보가 들어있는지 확인
+        console.log('product', product)
+
+        return <Col lg={6} md={8} xs={24} key={index}>
+        
+            <Card
+                cover={<img style={{ width: '100%', maxHeight: '200px' }} src={`http://localhost:5000/${product.images[0]}`} />}
+            >
+                <Meta
+                    title={product.title}
+                    description={`$${product.price}`}
+                />
+
+            </Card>
+        </Col>
+
+    })
 
     return (
         <div style={{ width: '75%', margin: '3rem auto' }}>
             <div style={{ textAlign: 'center' }}>
-                <h2>@jxxkive</h2>
+                <h2>@jxxkive <Icon type="rocket" /></h2>
             </div>
             
             {/* Filter */}
@@ -36,6 +61,9 @@ function LandingPage() {
 
             {/* Cards */}
 
+            <Row gutter={[16, 16]}>
+                {renderCards}
+            </Row>
 
             <div style={{ justifyContent: 'center' }}>
                 <button>더보기</button>
