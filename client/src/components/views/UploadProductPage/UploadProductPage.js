9393eeb1bb3865c48a106b9ac5c5cbb88350bf00
parkcheodol <3611345@naver.com>
2020-08-16 17:07:01 +0900

    200816 #2-9 onSubmit

--- client/src/components/views/UploadProductPage/UploadProductPage.js
+++ client/src/components/views/UploadProductPage/UploadProductPage.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react'
 import { Typography, Button, Form, Input } from 'antd';
 // #2-4 DropZone
 import FileUpload from '../../utils/FileUpload'
+import axios from 'axios';
+
 
 const { Title } = Typography;
 const { TextArea } = Input;
@@ -16,7 +18,7 @@ const Category = [
     { key: 6, value: "Cute" }
 ]
 
-function UploadProductPage() {
+function UploadProductPage(props) {
 
 
     // #2-2 6:00
@@ -48,13 +50,48 @@ function UploadProductPage() {
         setProductImages(newImages)
     }
 
+    // #2-9 onSubmit Func
+    const submitHandler = (event) => {
+        event.preventDefault();
+
+        // 모든 칸이 채워지지 않으면 제출할 수 없게
+        if (!ProductTitle || !ProductDescription || !ProductPrice || !ProductCategory || !ProductImages.length === 0 ) {
+            return alert("모든 값을 넣어주세요")
+        }
+
+        // 서버에 채운 값들을 request 보낸다
+
+        const body = {
+
+            // 로그인 한 사람의 아이디
+            writer: props.user.userData._id,
+            title: ProductTitle,
+            description: ProductDescription,
+            price: ProductPrice,
+            images: ProductImages,
+            category: ProductCategory
+        }
+
+        axios.post("/api/product", body)
+            .then(response => {
+                if(response.data.success) {
+                    
+                    alert('상품 업로드 성공')  
+                    props.history.push('/')
+                } else {
+                    alert('상품 업로드 실패')
+                }
+            })
+
+    }
+
     return (
         <div style={{ maxWidth: '700px', margin: '2rem auto' }}>
             <div style={{ textAlign: 'center', marginBottom: '2rem' }}>
                 <Title level={2}> 의류 상품 업로드 </Title>
             </div>
 
-            <Form> 
+            <Form onSubmit={submitHandler}> 
                 {/* DropZone */}
 
                 <FileUpload refreshFunction={updateProductImages} />
@@ -84,9 +121,9 @@ function UploadProductPage() {
 
                 <br />
                 <br />
-                <Button>
+                <button type="submit">
                     확인
-                </Button>
+                </button>
 
             </Form>
 
